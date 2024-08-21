2cb92327c9bccbdf222451adec1abbbc4cb2fccf
noreeya <notenarak1@gmail.com>
2024-08-21 14:55:54 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import streamlit as st
 import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.svm import SVC
 
@@ -14,7 +17,7 @@ y = df['species']
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
 
 st.sidebar.title('Classifiers')
-classifier = st.sidebar.selectbox('Select Classifier', ('KNN', 'SVM'))
+classifier = st.sidebar.selectbox('Select Classifier', ('KNN', 'SVM','DT','RF','NN'))
 if classifier == 'KNN':
   knn = KNeighborsClassifier(n_neighbors=3)
   knn.fit(x_train, y_train)
@@ -26,4 +29,22 @@ if classifier == 'SVM':
   svm.fit(x_train, y_train)
   y_pred = svm.predict(x_test)
   acc = accuracy_score(y_test, y_pred)
+  st.write(acc)
+if classifier == 'DT':
+  dt = DecisionTreeClassifier()
+  dt.fit(x_train, y_train)
+  y_pred = dt.predict(x_test)
+  accuracy_score(y_test, y_pred)
+  st.write(acc)
+if classifier == 'RF':
+  rf = RandomForestClassifier()
+  rf.fit(x_train, y_train)
+  y_pred = rf.predict(x_test)
+  accuracy_score(y_test, y_pred)
+  st.write(acc)
+if classifier == 'NN':
+  nn = MLPClassifier(hidden_layer_sizes=(100,100,100))
+  nn.fit(x_train, y_train)
+  y_pred = nn.predict(x_test)
+  accuracy_score(y_test, y_pred)
   st.write(acc)
