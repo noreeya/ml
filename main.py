6e34089a24ddb70d07d56c9fd83933ad8c83fdc8
noreeya <notenarak1@gmail.com>
2024-08-21 14:49:25 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
 import streamlit as st
 import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.svm import SVC
+
 df = sns.load_dataset('iris')
 df
-st.sidebar.title('Classifier')
-classifier = st.sidebar.selectbox('Classifier', ('KNN', 'SVM', 'DT', 'RF', 'NN'))
+
+x = df.iloc[:, :-1]
+y = df['species']
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+
+st.sidebar.title('Classifiers')
+classifier = st.sidebar.selectbox('Select Classifier', ('KNN', 'SVM'))
+if classifier == 'KNN':
+  knn = KNeighborsClassifier(n_neighbors=3)
+  knn.fit(x_train, y_train)
+  y_pred = knn.predict(x_test)
+  acc = accuracy_score(y_test, y_pred)
+  st.write(acc)
+if classifier == 'SVM':
+  svm = SVC()
+  svm.fit(x_train, y_train)
+  y_pred = svm.predict(x_test)
+  acc = accuracy_score(y_test, y_pred)
+  st.write(acc)
